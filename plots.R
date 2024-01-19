9ff1ca72fb7b5eee1b16d84ed96b3c0a03c4878e
wes592 <wes592@aol.com>
2024-01-18 16:08:25 -0800

    Create plotting example
    Added new script to generate plots and created them.

---
+++ plots.R
@@ -0,0 +1,13 @@
+### Example Plots
+
+pdf(file = "~/Documents/GitHub/cl_gitTutorial/data/plot1.pdf")
+plot(1:10)
+dev.off()
+
+pdf(file = "~/Documents/GitHub/cl_gitTutorial/data/plot2.pdf")
+plot(1:100)
+dev.off()
+
+pdf(file = "~/Documents/GitHub/cl_gitTutorial/data/plot3.pdf")
+plot(1:100, main = "plot 3")
+dev.off()
