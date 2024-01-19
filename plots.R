d81ebc2716b97a95d42e916fb372c1c7a8ebd2dd
wrhorton592 <wrhorton592@gmail.com>
2024-01-19 08:39:25 -0800

    Mistake in update to 3rd plot path. Fixed.

--- plots.R
+++ plots.R
@@ -11,6 +11,6 @@ pdf(file = file.path(dir_v, "plot2.pdf"))
 plot(1:(100*multiplier_v))
 dev.off()
 
-pdf(file = "~/Documents/GitHub/cl_gitTutorial/data/plot3.pdf")
+pdf(file = file.path(dir_v, "plot3.pdf"))
 plot(1:(100*multiplier_v), main = "plot 3")
 dev.off()
